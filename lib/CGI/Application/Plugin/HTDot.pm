5754e7ceff57f0491a629ef3626844a0d3504440
Jason A. Crome <cromedome@gmail.com>
2005-08-18 23:09:31 +0000

    Updated change log and version number for public release.

--- lib/CGI/Application/Plugin/HTDot.pm
+++ lib/CGI/Application/Plugin/HTDot.pm
@@ -10,11 +10,11 @@ templating mechanism.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-$CGI::Application::Plugin::HTDot::VERSION = '0.02';
+$CGI::Application::Plugin::HTDot::VERSION = '0.03';
 
 =head1 SYNOPSIS
 
